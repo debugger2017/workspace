c1cd1d9d1f72a8bed29640379d596084860388ff
debugger2017 <kartik.p.satoskar@gmail.com>
2017-01-16 09:55:40 +0530

    Fixed bug

--- sample_app/test/controllers/static_pages_controller_test.rb
+++ sample_app/test/controllers/static_pages_controller_test.rb
@@ -11,18 +11,18 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title" , "Help | Ruby on rails tutorial sample app"
+    assert_select "title" , "Ruby on rails tutorial sample app"
   end
 
   test "should get about" do
   	get static_pages_about_url
   	assert_response :success
-    assert_select "title" , "About | Ruby on rails tutorial sample app"
+    assert_select "title" , "Ruby on rails tutorial sample app"
   end
 
   test "should get contact" do
     get static_pages_contact_url
     assert_response :success
-    assert_select "title" , "Contact | Ruby on rails tutorial sample app"
+    assert_select "title" , "Ruby on rails tutorial sample app"
   end
 end
