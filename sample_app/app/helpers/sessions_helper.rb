034391724bd1a6491b4042714572d6c90c48ffda
debugger2017 <kartik.p.satoskar@gmail.com>
2017-01-23 19:07:04 +0530

    Forgetting persistent session

--- sample_app/app/helpers/sessions_helper.rb
+++ sample_app/app/helpers/sessions_helper.rb
@@ -20,6 +20,7 @@ module SessionsHelper
 	end
 
 	def log_out
+		forget(current_user)
 		session.delete(:user_id)
 		@current_user = nil
 	end
@@ -30,5 +31,9 @@ module SessionsHelper
 		cookies.permanent[:remember_token] = user.remember_token
 	end
 
-	
+	def forget(user)
+		user.forget
+		cookies.delete(:user_id)
+		cookies.delete(:remember_token)
+	end	
 end
