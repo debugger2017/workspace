94fd5f2627cff4598253c8e2d0d3f669859e6206
debugger2017 <kartik.p.satoskar@gmail.com>
2017-01-23 17:48:17 +0530

    Added logout facility

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
 
   post '/login' , to: 'sessions#create' 
 
-  post '/logout' , to: 'sessions#destroy'
+  delete '/logout' , to: 'sessions#destroy'
   
   resources :users
 end
