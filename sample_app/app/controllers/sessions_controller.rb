2b1139602eff08da2cea4630f47950160e1a6ec2
debugger2017 <kartik.p.satoskar@gmail.com>
2017-01-23 15:34:49 +0530

    Authenticated create action

--- sample_app/app/controllers/sessions_controller.rb
+++ sample_app/app/controllers/sessions_controller.rb
@@ -3,7 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	render 'new'
+  	user = User.find_by(email: params[:session][:email].downcase)
+  	if user && user.authenticate(params[:session][:password])
+  	
+  	else		
+  		render 'new'
   end
+
+  def destroy
+  end
+  
 
 end
