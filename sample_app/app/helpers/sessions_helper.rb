d6df76ff55022f425d8712ebeaaadb39bfe0671c
debugger2017 <kartik.p.satoskar@gmail.com>
2017-01-23 19:01:35 +0530

    Added for persistent sessions

--- sample_app/app/helpers/sessions_helper.rb
+++ sample_app/app/helpers/sessions_helper.rb
@@ -4,7 +4,15 @@ module SessionsHelper
 	end
 
 	def current_user
-		@current_user ||= User.find_by(id: session[:user_id])
+		if (user_id = session[:user_id])
+			@current_user ||= User.find_by(id: user_id)
+		elsif (user_id = cookies.signed[:user_id])
+			user = User.find_by(id: user_id)
+			if user && user.authenticate?(cookies[:remember_token])
+				log_in user
+				@current_user = user
+			end
+		end
 	end
 
 	def logged_in?
@@ -21,6 +29,6 @@ module SessionsHelper
 		cookie.permanent.signed[:user_id] = user.id
 		cookies.permanent[:remember_token] = user.remember_token
 	end
-	
+
 	
 end
