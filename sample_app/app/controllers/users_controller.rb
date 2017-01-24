0eaedc574933bc4cf4c69d4f6e7787389b47089a
debugger2017 <kartik.p.satoskar@gmail.com>
2017-01-24 15:02:57 +0530

    Added before action

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  
+  before_action :logged_in_user, only: [:edit,:update]
+
   def show
   	@user = User.find(params[:id])
   	#debugger
@@ -38,5 +39,13 @@ class UsersController < ApplicationController
 
   	def user_params
   		params.require(:user).permit(:name, :email,:password,:password_confirmation)
-  	end 			
+  	end 		
+
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to logged_in_url
+      end
+    end
+
 end
