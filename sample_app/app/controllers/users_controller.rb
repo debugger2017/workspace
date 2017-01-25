ed8169d72a40872ca297903413a09a00105b0d62
debugger2017 <kartik.p.satoskar@gmail.com>
2017-01-25 16:18:02 +0530

    Successfully tested

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
-      UserMailer.account_activation(@user).deliever_now
+      UserMailer.account_activation(@user).deliver_now
       log_in @user
       flash[:info] = "Please check your email to activate your account."
       redirect_to root_url
