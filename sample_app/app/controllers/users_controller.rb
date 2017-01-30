88972484125927f55aa8213cf9e08d3127dd3112
debugger2017 <kartik.p.satoskar@gmail.com>
2017-01-30 11:37:09 +0530

    Added show for microposts

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   def show
   	@user = User.find(params[:id])
-  	#debugger
+  	@microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def index
