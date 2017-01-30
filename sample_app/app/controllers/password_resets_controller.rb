6f0a638d9e6d70e415776aafa08a62ae97134497
debugger2017 <kartik.p.satoskar@gmail.com>
2017-01-30 10:43:41 +0530

    Fixed bug

--- sample_app/app/controllers/password_resets_controller.rb
+++ sample_app/app/controllers/password_resets_controller.rb
@@ -1,7 +1,7 @@
 class PasswordResetsController < ApplicationController
-  before_action :get_user, only: [:edit, :update]
-  before_action :valid_user, only:[:edit,:update]
-  before action :check_expiration, only: [:edit,:update]
+  before_action :get_user,         only: [:edit, :update]
+  before_action :valid_user,       only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]    # Case (1)
 
   def new
   end
@@ -23,41 +23,43 @@ class PasswordResetsController < ApplicationController
   end
 
   def update
-    if params[:user][:password].empty?
-      @user.errors.add(:password,"cant' empty")
+    if params[:user][:password].empty?                  # Case (3)
+      @user.errors.add(:password, "can't be empty")
       render 'edit'
-    elsif @user.update_attributes(user_params)
-          log_in @user 
-          flash[:success] = "Password has been reset"
-          redirect_to @user
+    elsif @user.update_attributes(user_params)          # Case (4)
+      log_in @user
+      flash[:success] = "Password has been reset."
+      redirect_to @user
     else
-      render 'edit'
+      render 'edit'                                     # Case (2)
     end
   end
 
   private
 
     def user_params
-      params.require(:user).permit(:password , :password_confirmation)
+      params.require(:user).permit(:password, :password_confirmation)
     end
+
+    # Before filters
 
     def get_user
       @user = User.find_by(email: params[:email])
     end
 
+    # Confirms a valid user.
     def valid_user
-      unless (@user && @user.activated? && @user.authenticated?(:reset,params[:id]))
+      unless (@user && @user.activated? &&
+              @user.authenticated?(:reset, params[:id]))
         redirect_to root_url
       end
     end
 
+    # Checks expiration of reset token.
     def check_expiration
       if @user.password_reset_expired?
         flash[:danger] = "Password reset has expired."
         redirect_to new_password_reset_url
       end
     end
-
-
-
 end
--- sample_app/test/integration/password_resets_test.rb
+++ sample_app/test/integration/password_resets_test.rb
@@ -1,9 +1,10 @@
 require 'test_helper'
 
 class PasswordResetsTest < ActionDispatch::IntegrationTest
+
   def setup
-  	ActionMailer::Base.deliveries.clear
- 	@user = users(:michael)
+    ActionMailer::Base.deliveries.clear
+    @user = users(:michael)
   end
 
   test "password resets" do
